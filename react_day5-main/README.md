44bd3b6d59b92c700ce5796c45060dd31e7df551
kevin andrews <kevinandrews001@gmail.com>
2023-08-02 08:39:40 +0530

    Guvi day25 task

---
+++ react_day5-main/README.md
@@ -0,0 +1,1 @@
+Task for React Day 5 - CRUD operation
---
+++ react_day5-main/src/UserList.js
@@ -0,0 +1,21 @@
+const userList = [
+    {
+        id: 0,
+        userName: 'Cristiano Ronaldo',
+        profile: 'CR7'
+    },
+    {
+        id: 1,
+        userName: 'leonal Messi',
+        profile: 'Messi'
+    },
+    {
+        id: 2,
+        userName: 'sunil Chethri',
+        profile: 'Sunil'
+
+    }
+
+];
+
+export default userList;
